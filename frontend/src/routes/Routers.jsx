0fb6e6f079e3bce2803097a5e6e48c81234be77f
Asen Chochev <asen.qkiqt145@gmail.com>
2025-09-15 20:43:30 +0300

    Update Routers.jsx

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -6,15 +6,17 @@ import Register from '../pages/Register'
 import Doctors from '../pages/Doctors/Doctors'
 import DoctorsDetails from '../pages/Doctors/DoctorsDetails'
 
+import {Routes, Route} from 'react-router-dom'
+
 const Routers = () => {
-  return <>
-    <Home />
-    <Services />
-    <Login />
-    <Register />
-    <Doctors />
-    <DoctorsDetails />
-  </>
+  return <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/services' element={<Services/>}/>
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/register' element={<Register/>}/>
+    <Route path='/doctors' element={<Doctors/>}/>
+    <Route path='/doctors/:id' element={<DoctorsDetails/>}/>
+  </Routes>
 }
 
 export default Routers
