78540063847a33f86bcb7fcc080bf5f3955c7438
Asen Chochev <asen.qkiqt145@gmail.com>
2025-09-19 23:51:05 +0300

    Upload Logo and Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import {useEffect, useRef} from 'react'
+import logo from '../../assets/images/logo.png'
+import {NavLink, Link} from 'react-router-dom'
+
+const navLinks = [
+  {
+    path: '/home',
+    display: 'Home'
+  },
+  {
+    path: '/doctors',
+    display: 'Find a Doctor'
+  },
+  {
+    path: '/services',
+    display: 'Services'
+  },
+  {
+    path: '/contact',
+    display: 'Contact'
+  }
+]
 
 const Header = () => {
-  return (
-    <div>
-      Header
+  return <header className='header flex items-center'>
+    <div className='container'>
+      <div className='flex items-center justify-between'>
+        {/* logo */}
+        <div>
+          <img src={logo} alt="" />
+        </div>
+
+        {/* navigation menu */}
+        <div className='navigation'>
+          <ul className='menu flex items-center gap-[2.7rem]'>
+            {
+              navLinks.map((link, index)=><li key={index} className='navItem'>
+                <NavLink to={link.path} className={navClass=>navClass.isActive ? 'active' : ''}>{link.display}</NavLink>
+              </li>)
+            }
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+    
+  </header>
+  
 }
 
 export default Header
